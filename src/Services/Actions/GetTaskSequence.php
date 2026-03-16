0955e60381f05805d067ea119bc6c4c5eb99a06a
Opscale <development@opscale.co>
2026-03-16 08:28:34 -0600

    fix(gettasksequence): fixed bug when generating unique sequence

--- src/Services/Actions/GetTaskSequence.php
+++ src/Services/Actions/GetTaskSequence.php
@@ -4,6 +4,7 @@ namespace Opscale\NovaServiceDesk\Services\Actions;
 
 use Opscale\Actions\Action;
 use Opscale\NovaServiceDesk\Models\Subcategory;
+use Opscale\NovaServiceDesk\Models\Task;
 
 class GetTaskSequence extends Action
 {
@@ -57,8 +58,10 @@ class GetTaskSequence extends Action
             ];
         }
 
-        $lastRequest = $subcategory->requests()->withoutGlobalScopes()->orderByDesc('key')->first();
-        $lastNumber = $lastRequest ? (int) substr($lastRequest->key, strrpos($lastRequest->key, '-') + 1) : 0;
+        $lastTask = Task::whereHas('request', fn ($query) => $query->where('subcategory_id', $subcategory->id))
+            ->orderByDesc('key')
+            ->first();
+        $lastNumber = $lastTask ? (int) substr($lastTask->key, strrpos($lastTask->key, '-') + 1) : 0;
         $correlative = str_pad($lastNumber + 1, 6, '0', STR_PAD_LEFT);
         $sequence = $identifier . '-' . $correlative;
 
